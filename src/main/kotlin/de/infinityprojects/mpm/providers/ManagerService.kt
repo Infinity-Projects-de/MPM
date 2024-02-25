5e9d1bf1bb8a3dacab4d227276317f6109ae194b
Chechu <22799163+ChechuDEV@users.noreply.github.com>
2024-02-25 18:57:53 +0100

    feat: create manager provider for the api

---
+++ src/main/kotlin/de/infinityprojects/mpm/providers/ManagerService.kt
@@ -0,0 +1,41 @@
+package de.infinityprojects.mpm.providers
+
+import de.infinityprojects.mpm.api.Manager
+import de.infinityprojects.mpm.api.Registry
+import de.infinityprojects.mpm.item.Item
+import de.infinityprojects.mpm.utils.Registers
+
+class ManagerService: Manager {
+    private var registers: MutableMap<Registers, Registry<*>> = mutableMapOf()
+
+    override fun <T> getRegistry(t: Class<T>): Registry<T> {
+        val registry = registers.values.find { it.isType(t) } as? Registry<T>
+        if (registry == null) {
+            throw IllegalArgumentException("Registry does not exist")
+        }
+        return registry
+    }
+
+    override fun getItemRegistry(): Registry<Item> {
+        return getRegistry(Item::class.java)
+    }
+
+    fun getRegistry(r: Registers): Registry<*> {
+        val registry = registers[r] ?: throw IllegalArgumentException("Registry does not exist")
+        return registry
+    }
+
+    fun declareRegistry(r: Registers, registry: Registry<*>) {
+        val reg = registers[r]
+        if (reg != null) {
+            throw IllegalArgumentException("Registry already exists")
+        }
+        registers[r] = registry
+    }
+
+    private fun getRegistries(): Map<Registers, Registry<*>> {
+        return registers.toMap()
+    }
+
+
+}
