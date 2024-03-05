202ad81986c7c3e75e51db8d296239cacb3b1b99
Chechu <22799163+ChechuDEV@users.noreply.github.com>
2024-03-05 14:36:53 +0100

    feat: add automatic block texture creation and register new listeners

--- src/main/kotlin/de/infinityprojects/mpm/Main.kt
+++ src/main/kotlin/de/infinityprojects/mpm/Main.kt
@@ -3,11 +3,17 @@ package de.infinityprojects.mpm
 import com.google.gson.JsonArray
 import com.google.gson.JsonObject
 import de.infinityprojects.mpm.api.Manager
-import de.infinityprojects.mpm.api.Registry
+import de.infinityprojects.mpm.block.Block
+import de.infinityprojects.mpm.block.handler.BlockHandler
+import de.infinityprojects.mpm.block.handler.BlockProvider
+import de.infinityprojects.mpm.block.handler.BlockRenderer
+import de.infinityprojects.mpm.block.handler.NoteblockDisabler
+import de.infinityprojects.mpm.block.handler.PlaceListener
+import de.infinityprojects.mpm.block.storage.Region
 import de.infinityprojects.mpm.item.Item
-import de.infinityprojects.mpm.item.ItemHandler
+import de.infinityprojects.mpm.item.handler.ItemHandler
+import de.infinityprojects.mpm.item.handler.ItemProvider
 import de.infinityprojects.mpm.packet.PacketHandler
-import de.infinityprojects.mpm.providers.ItemProvider
 import de.infinityprojects.mpm.providers.ManagerService
 import de.infinityprojects.mpm.providers.RegistryProvider
 import de.infinityprojects.mpm.textures.PlayerJoinResourcePack
@@ -21,11 +27,20 @@ import org.bukkit.event.Listener
 import org.bukkit.plugin.Plugin
 import org.bukkit.plugin.ServicePriority
 import org.bukkit.plugin.java.JavaPlugin
+import java.util.concurrent.TimeUnit
 
 class Main : JavaPlugin() {
     override fun onEnable() {
         val manager = ManagerService()
-        manager.declareRegistry(Registers.ITEM, Registry.createRegistry(Item::class.java))
+        manager.declareRegistry(Registers.ITEM, RegistryProvider(Item::class.java, 1000))
+        manager.declareRegistry(
+            Registers.BLOCK,
+            RegistryProvider(Block::class.java, BlockProvider.BLOCK_MODEL_OFFSET, BlockProvider.BLOCK_MODEL_OFFSET + 25 * 23 * 2),
+        ) // TODO Check if this is the right maxID
+
+        if (ItemProvider.MATERIAL.isBlock || BlockProvider.MATERIAL.isBlock) {
+            throw IllegalArgumentException("Material cannot be a block for providers")
+        }
 
         server.servicesManager.register(Manager::class.java, manager, this, ServicePriority.Normal)
 
@@ -34,67 +49,114 @@ class Main : JavaPlugin() {
         command?.setExecutor(c)
         command?.tabCompleter = c
 
-        server.pluginManager.registerEvents(PacketHandler, this)
+        registerListener(PacketHandler)
         PacketHandler.registerListeners(ItemHandler())
+        PacketHandler.registerListeners(BlockHandler(this))
+        registerListener(PlaceListener())
+        registerListener(BlockRenderer())
+        registerListener(NoteblockDisabler())
 
         // Test methods not MPM
         Item.registerItem(this, "portal", null)
+        Block.registerBlock(this, "aether_dirt", null, false)
+        Block.registerBlock(this, "aether_grass_block", null, true)
         // END Test methods
 
-        Bukkit.getScheduler().runTaskLater(this, Runnable {
-            createTextures(manager)
-        }, 1L)
+        Bukkit.getScheduler().runTaskLater(
+            this,
+            Runnable {
+                createTextures(manager)
+            },
+            1L,
+        )
+
+        Bukkit.getAsyncScheduler().runAtFixedRate(this, {
+            Region.saveAll()
+        }, 5, 5, TimeUnit.MINUTES)
     }
 
-    private fun createItems(manager: Manager, zip: ZipCreator) {
-        val itemRegistry = manager.getItemRegistry() as RegistryProvider<Item>
-        val plugins = mutableMapOf<String, Plugin>()
-        val models = mutableMapOf<Int, String>()
+    override fun onDisable() {
+        Region.saveAll()
+    }
 
-        itemRegistry.getAll().forEach {
-            val pluginName = it.key.namespace()
-            if (!plugins.contains(pluginName)) {
-                plugins[pluginName] = server.pluginManager.getPlugin(pluginName)
-                    ?: throw IllegalArgumentException("Plugin $pluginName does not exist")
-            }
-            val plugin = plugins[pluginName]!!
+    val plugins = mutableMapOf<String, Plugin>()
 
-            val itemName = it.key.key
-            val item = it.value as ItemProvider
+    private fun getPlugin(name: String): Plugin {
+        if (!plugins.contains(name)) {
+            plugins[name] = server.pluginManager.getPlugin(name)
+                ?: throw IllegalArgumentException("Plugin $name does not exist")
+        }
+        return plugins[name]!!
+    }
 
-            val file = plugin.getResource("textures/item/$itemName.png")
-                ?: throw IllegalArgumentException("Texture for $itemName does not exist")
+    private fun createBlocks(
+        manager: Manager,
+        zip: ZipCreator,
+    ) {
+        val blockRegistry = manager.getBlockRegistry() as RegistryProvider<Block>
+        val overrides = JsonArray()
+        val variants = JsonObject()
 
-            zip.writeStream(file, "assets/$pluginName/textures/item/$itemName.png")
-            models[item.model] = "$pluginName:item/$itemName"
+        blockRegistry.getAll().map { it.value as BlockProvider }.forEach {
+            val pluginName = it.getPluginName()
+            val plugin = getPlugin(pluginName)
+            val blockName = it.id.key
 
-            val modelJson = JsonObject()
-            modelJson.addProperty("parent", "item/handheld")
-            val textures = JsonObject()
-            textures.addProperty("layer0", "$pluginName:item/$itemName")
-            modelJson.add("textures", textures)
+            val file =
+                plugin.getResource("textures/block/$blockName.png")
+                    ?: throw IllegalArgumentException("Texture for $blockName does not exist")
 
-            val bArray = modelJson.toString().toByteArray()
+            zip.writeStream(file, "assets/$pluginName/textures/block/$blockName.png")
 
-            zip.writeData(bArray, "assets/$pluginName/models/item/$itemName.json")
+            zip.writeData(it.getModelObject(), "assets/$pluginName/models/block/$blockName.json")
+            overrides.add(it.getOverrideObject())
+
+            variants.add(it.model.variantName, it.getVariant())
         }
 
-        val modelJson = JsonObject()
-        modelJson.addProperty("parent", "minecraft:block/stone")
+        val blockMaterial = BlockProvider.MATERIAL.name.lowercase()
+
+        val overrideJson = JsonObject()
+        overrideJson.addProperty("parent", "minecraft:item/$blockMaterial")
+        overrideJson.add("overrides", overrides)
+
+        zip.writeData(overrideJson, "assets/minecraft/models/item/$blockMaterial.json")
+
+        val variantJson = JsonObject()
+        variantJson.add("variants", variants)
+
+        zip.writeData(variantJson, "assets/minecraft/blockstates/note_block.json")
+    }
+
+    private fun createItems(
+        manager: Manager,
+        zip: ZipCreator,
+    ) {
+        val itemRegistry = manager.getItemRegistry() as RegistryProvider<Item>
         val overrides = JsonArray()
-        models.forEach {
-            val override = JsonObject()
-            val predicate = JsonObject()
-            predicate.addProperty("custom_model_data", it.key)
-            override.add("predicate", predicate)
-            override.addProperty("model", it.value)
-            overrides.add(override)
-        } // todo put in main loop
-        modelJson.add("overrides", overrides)
 
-        val bArray = modelJson.toString().toByteArray()
-        zip.writeData(bArray, "assets/minecraft/models/item/stone.json")
+        itemRegistry.getAll().map { it.value as ItemProvider }.forEach {
+            val pluginName = it.getPluginName()
+            val plugin = getPlugin(pluginName)
+            val itemName = it.id.key
 
+            val file =
+                plugin.getResource("textures/item/$itemName.png")
+                    ?: throw IllegalArgumentException("Texture for $itemName does not exist")
+
+            zip.writeStream(file, "assets/$pluginName/textures/item/$itemName.png")
+
+            zip.writeData(it.getModelObject(), "assets/$pluginName/models/item/$itemName.json")
+            overrides.add(it.getOverrideObject())
+        }
+
+        val itemMaterial = ItemProvider.MATERIAL.name.lowercase()
+
+        val overrideJson = JsonObject()
+        overrideJson.addProperty("parent", "minecraft:item/$itemMaterial")
+        overrideJson.add("overrides", overrides)
+
+        zip.writeData(overrideJson, "assets/minecraft/models/item/$itemMaterial.json")
     }
 
     fun createTextures(manager: Manager) {
@@ -103,13 +165,15 @@ class Main : JavaPlugin() {
         val plugins: MutableMap<String, Plugin> = mutableMapOf()
 
         createItems(manager, creator)
+        createBlocks(manager, creator)
 
-        val packData = this.getResource("pack.mcmeta")
-            ?: throw IllegalArgumentException("pack.mcmeta does not exist")
-        creator.writeStream(packData, "pack.mcmeta")
-        val packImage = this.getResource("pack.png")
-            ?: throw IllegalArgumentException("pack.png does not exist")
-        creator.writeStream(packImage, "pack.png")
+        val rootCopy = listOf("pack.mcmeta", "pack.png")
+        rootCopy.forEach {
+            val file =
+                this.getResource(it)
+                    ?: throw IllegalArgumentException("$it does not exist")
+            creator.writeStream(file, it)
+        }
 
         creator.close()
 
