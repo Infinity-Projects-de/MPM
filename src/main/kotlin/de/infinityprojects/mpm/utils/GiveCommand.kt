32fa3991861c8eafa708598989558cb455e14806
Chechu <22799163+ChechuDEV@users.noreply.github.com>
2024-03-05 14:31:57 +0100

    fix: fix typo in onCommand, which didn't allow the correct registry to get called

--- src/main/kotlin/de/infinityprojects/mpm/utils/GiveCommand.kt
+++ src/main/kotlin/de/infinityprojects/mpm/utils/GiveCommand.kt
@@ -13,7 +13,7 @@ class GiveCommand() : TabExecutor {
         sender: CommandSender,
         command: Command,
         label: String,
-        args: Array<out String>?
+        args: Array<out String>?,
     ): MutableList<String> {
         val manager = Manager.getManager()
 
@@ -27,11 +27,14 @@ class GiveCommand() : TabExecutor {
             return mutableListOf()
         }
 
-        val registry = (if (args[0] == "item") {
-            manager.getItemRegistry()
-        } else {
-            manager.getBlockRegistry()
-        }) as RegistryProvider
+        val registry =
+            (
+                if (args[0] == "item") {
+                    manager.getItemRegistry()
+                } else {
+                    manager.getBlockRegistry()
+                }
+            ) as RegistryProvider
 
         if (args.size == 2) {
             val plugins = registry.getAll().keys.map { it.namespace() }.toSet()
@@ -47,14 +50,24 @@ class GiveCommand() : TabExecutor {
         return mutableListOf()
     }
 
-    override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>?): Boolean {
+    override fun onCommand(
+        sender: CommandSender,
+        command: Command,
+        label: String,
+        args: Array<out String>?,
+    ): Boolean {
         if (args?.size == 3) {
             val player = sender as? Player ?: return false
 
             val manager = Manager.getManager()
             val registry =
-                (if (args[0] == "item") manager.getItemRegistry()
-                else manager.getBlockRegistry()) as RegistryProvider
+                (
+                    if (args[0] == "item") {
+                        manager.getItemRegistry()
+                    } else {
+                        manager.getBlockRegistry()
+                    }
+                ) as RegistryProvider
 
             val plugin = Bukkit.getPluginManager().getPlugin(args[1]) ?: return false
             val item = registry.get(plugin, args[2])
