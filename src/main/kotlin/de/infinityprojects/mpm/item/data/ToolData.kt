b4f6906e7ef39ef9161535c54f8760cfeada514b
Chechu <22799163+ChechuDEV@users.noreply.github.com>
2024-03-05 14:40:36 +0100

    refactor: split tool type

--- src/main/kotlin/de/infinityprojects/mpm/item/data/ToolData.kt
+++ src/main/kotlin/de/infinityprojects/mpm/item/data/ToolData.kt
@@ -4,16 +4,7 @@ class ToolData(
     val toolType: ToolType = ToolType.PICKAXE,
     val durability: Int = 0,
     val miningLevel: Int = 0,
-    val miningSpeed: Float = 0.0f,
+    val miningSpeed: Float = 1.0f,
     val attackDamage: Float = 0.0f,
     val attackSpeed: Float = 0.0f,
-): ItemData(ItemType.TOOL) {
-
-    enum class ToolType {
-        PICKAXE,
-        AXE,
-        SHOVEL,
-        HOE,
-        SWORD,
-    }
-}
+) : ItemData(ItemType.TOOL)
---
+++ src/main/kotlin/de/infinityprojects/mpm/item/data/ToolType.kt
@@ -0,0 +1,10 @@
+package de.infinityprojects.mpm.item.data
+
+enum class ToolType {
+    PICKAXE,
+    AXE,
+    SHOVEL,
+    HOE,
+    SWORD,
+    NONE,
+}
