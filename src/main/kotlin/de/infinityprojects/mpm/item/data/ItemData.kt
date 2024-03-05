848f261194bdc88eec08d1cf9e4e06c4cffacda6
Chechu <22799163+ChechuDEV@users.noreply.github.com>
2024-03-05 14:33:53 +0100

    feat: create an anonymous ItemData class for items with no (or null) special data

--- src/main/kotlin/de/infinityprojects/mpm/item/data/ItemData.kt
+++ src/main/kotlin/de/infinityprojects/mpm/item/data/ItemData.kt
@@ -1,5 +1,9 @@
 package de.infinityprojects.mpm.item.data
 
 abstract class ItemData(val type: ItemType = ItemType.ITEM) {
-
+    companion object {
+        fun simple(): ItemData {
+            return object : ItemData() {}
+        }
+    }
 }
