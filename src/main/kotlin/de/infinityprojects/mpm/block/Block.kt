1c58a47d9d75eb3b87abe1e12bddd586331f60d7
Chechu <22799163+ChechuDEV@users.noreply.github.com>
2024-03-05 14:26:03 +0100

    feat: Created block interface and register method

--- src/main/kotlin/de/infinityprojects/mpm/block/Block.kt
+++ src/main/kotlin/de/infinityprojects/mpm/block/Block.kt
@@ -1,4 +1,34 @@
 package de.infinityprojects.mpm.block
 
-interface Block {
+import de.infinityprojects.mpm.api.Manager
+import de.infinityprojects.mpm.block.handler.BlockProvider
+import de.infinityprojects.mpm.item.Item
+import org.bukkit.Location
+import org.bukkit.plugin.Plugin
+
+interface Block : Item {
+    fun setBlock(location: Location)
+
+    fun getBlockData(): BlockData
+
+    companion object {
+        fun registerBlock(
+            plugin: Plugin,
+            id: String,
+            data: BlockData? = null,
+            multisided: Boolean = false,
+        ) {
+            val block = BlockProvider.createBlock(plugin, id, data, multisided)
+            Manager.getManager().getBlockRegistry().register(plugin, id, block)
+        }
+
+        fun registerItems(
+            plugin: Plugin,
+            items: Map<String, BlockData?>,
+        ) {
+            items.forEach { (id, data) ->
+                registerBlock(plugin, id, data, false) // TODO: Add multisided possibility to the map
+            }
+        }
+    }
 }
