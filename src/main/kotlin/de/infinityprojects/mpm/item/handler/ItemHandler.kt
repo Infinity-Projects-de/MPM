fa47a93af966b22dd442cc6fe66c537f707784eb
Chechu <22799163+ChechuDEV@users.noreply.github.com>
2024-03-07 13:10:14 +0100

    feat: dropped item entities are now properly rendered

--- src/main/kotlin/de/infinityprojects/mpm/item/handler/ItemHandler.kt
+++ src/main/kotlin/de/infinityprojects/mpm/item/handler/ItemHandler.kt
@@ -1,5 +1,6 @@
 package de.infinityprojects.mpm.item.handler
 
+import de.infinityprojects.mpm.Main
 import de.infinityprojects.mpm.api.Manager
 import de.infinityprojects.mpm.block.handler.BlockProvider
 import de.infinityprojects.mpm.item.mutable.MPMItem
@@ -7,16 +8,20 @@ import de.infinityprojects.mpm.item.mutable.MutableItem
 import de.infinityprojects.mpm.packet.PacketEvent
 import de.infinityprojects.mpm.packet.PacketListener
 import net.minecraft.core.NonNullList
+import net.minecraft.network.protocol.game.ClientboundBundlePacket
 import net.minecraft.network.protocol.game.ClientboundContainerSetContentPacket
 import net.minecraft.network.protocol.game.ClientboundContainerSetSlotPacket
+import net.minecraft.network.protocol.game.ClientboundSetEntityDataPacket
+import net.minecraft.network.syncher.EntityDataAccessor
+import net.minecraft.network.syncher.EntityDataSerializers
+import net.minecraft.network.syncher.SynchedEntityData.DataValue
 import net.minecraft.world.item.ItemStack
 import org.bukkit.Bukkit
 import org.bukkit.Material
 import org.bukkit.NamespacedKey
-import org.bukkit.entity.Player
 import org.bukkit.persistence.PersistentDataType
 
-class ItemHandler {
+class ItemHandler(val mpm: Main) {
     @PacketListener
     fun containerSetPacket(e: PacketEvent<ClientboundContainerSetContentPacket>) {
         val p = e.packet
@@ -24,12 +29,12 @@ class ItemHandler {
         val items = NonNullList.create<ItemStack>()
         var changes = false
         for (item in p.items) {
-            val rItem = getRealItem(e.player, item)
+            val rItem = getRealItem(item)
             if (rItem != item) changes = true
             items.add(rItem)
         }
 
-        val carriedItem = getRealItem(e.player, p.carriedItem)
+        val carriedItem = getRealItem(p.carriedItem)
         if (changes || carriedItem != p.carriedItem) {
             e.packet = ClientboundContainerSetContentPacket(p.containerId, p.stateId, items, carriedItem)
         }
@@ -38,16 +43,43 @@ class ItemHandler {
     @PacketListener
     fun itemSetPacket(e: PacketEvent<ClientboundContainerSetSlotPacket>) {
         val p = e.packet
-        val item = getRealItem(e.player, p.item)
+        val item = getRealItem(p.item)
         if (item != p.item) {
             e.packet = ClientboundContainerSetSlotPacket(p.containerId, p.stateId, p.slot, item)
         }
     }
 
-    private fun getRealItem(
-        player: Player,
-        item: ItemStack,
-    ): ItemStack {
+    @PacketListener
+    fun entityPacket(e: PacketEvent<ClientboundBundlePacket>) {
+        val b = e.packet
+        b.subPackets().forEach { p ->
+            if (p is ClientboundSetEntityDataPacket) {
+                handleMetadataPacket(p)
+            }
+        }
+    }
+
+    private fun handleMetadataPacket(p: ClientboundSetEntityDataPacket) {
+        p.packedItems.forEachIndexed { i, d ->
+            if (d.id == 8) {
+                val item = d.value
+                if (item is ItemStack) {
+                    val rItem = getRealItem(item)
+                    if (rItem != item) {
+                        val data = DataValue.create(EntityDataAccessor(8, EntityDataSerializers.ITEM_STACK), rItem)
+                        p.packedItems[i] = data
+                    }
+                }
+            }
+        }
+    }
+
+    @PacketListener
+    fun entityDataPacket(e: PacketEvent<ClientboundSetEntityDataPacket>) {
+        handleMetadataPacket(e.packet)
+    }
+
+    private fun getRealItem(item: ItemStack): ItemStack {
         val bukkitItem = item.asBukkitMirror()
         if (bukkitItem.type.isAir) return item
 
@@ -62,8 +94,8 @@ class ItemHandler {
 
             val id =
                 when (mutableItem.item) {
-                    is BlockProvider -> (mutableItem.item as BlockProvider).id
-                    is ItemProvider -> (mutableItem.item as ItemProvider).id
+                    is BlockProvider -> mutableItem.item.id
+                    is ItemProvider -> mutableItem.item.id
                     else -> NamespacedKey(Bukkit.getPluginManager().getPlugin("mpm")!!, "null")
                 }
 
@@ -103,7 +135,7 @@ class ItemHandler {
                     bukkitItem.type = itemStack.type
                     bukkitItem.itemMeta = itemStack.itemMeta
 
-                    return getRealItem(player, item)
+                    return getRealItem(item)
                 }
             }
 
